bd90a8e929f14e59d37c8832bb2942ccb64b9a85
Vincent007super <115561924+Vincent007super@users.noreply.github.com>
2025-11-28 16:48:09 +0100

    Updated landingspage with scrollable thing

--- crud_app/src/app/page.tsx
+++ crud_app/src/app/page.tsx
@@ -1,17 +1,32 @@
+import Header from "@/components/Header"
 
 export default function Home() {
   return (
     <>
+      <Header />
       <main>
-        <div className="pointer-events-none absolute inset-0 bg-gradient-to-b from-transparent via-black/65 to-black z-2">
+        <div className="pointer-events-none fixed inset-0 bg-gradient-to-b from-transparent via-black/65 to-black z-2">
         </div>
         <div className="relative w-full h-[820px]">
-          <video autoPlay loop muted className="w-full h-[820px] absolute object-cover opacity-60 ">
-            <source src= "/videos/waves.mp4" type="video/mp4" />
+          <video autoPlay loop muted className="w-full h-[820px] fixed object-cover opacity-60 ">
+            <source src="/videos/waves.mp4" type="video/mp4" />
           </video>
         </div>
-<h1 className="text-white font-bold z-3 absolute top-4/12 left-2/10 text-5xl"> Welcome to the <br /> Anti Procrastinator App (APA)</h1>
-<h1 className="text-white font-bold z-3 absolute top-9/20 right-2/10 text-2xl"> What will you do today?</h1>
+        <div className="fixed w-screen z-3 top-4/12 flex-col justify-center gap-5 flex justify-center items-center">
+          <h1 className="text-white font-bold z-3 text-5xl w-6/12"> Welcome to the <br /> Anti Procrastinator App (APA)</h1>
+          <h1 className="text-white font-bold z-3 text-2xl text-right w-5/12"> What will you do today?</h1>
+        </div>
+        <div className="w-screen bg-black h-[1600px] z-5 relative mt-[-200px] flex flex-col gap-6 justify-center">
+          <div className="w-8/12 flex h-6/20">
+
+          </div>
+          <div className="w-8/12 flex h-6/20">
+
+          </div>
+          <div className="w-8/12 flex h-6/20">
+
+          </div>
+        </div>
       </main>
     </>
   );
--- crud_app/src/components/Header.tsx
+++ crud_app/src/components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
         <>
-            <div className="w-full flex justify-center items-center h-30 absolute top-0">
+            <div className="z-15 w-full flex justify-center items-center h-20 fixed top-0">
                 <header className="w-250 h-15 bg-black z-15 flex relative justify-center items-center rounded-4xl translate-x-">
                     <h1 className="text-white font-bold text-2xl">Anti Procrastinator App (APA)</h1>
                     {/* Content here, eventually */}
